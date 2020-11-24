b54376a6b84eac1d5af0c19ac846ec737eb320be
luk101 <sethalbrecht16@gmail.com>
2020-11-23 21:00:26 -0800

    Dummy id values with Money, Time, and Sustainability categories

---
+++ get_rankings.js
@@ -0,0 +1,40 @@
+function get_rankings() {
+	/*Get First ranking information from algorithm
+		document.getElementById('travel_one').innerHTML = sessionStorage.???;
+		document.getElementById('money_one').innerHTML = sessionStorage.???;
+		document.getElementById('sust_one').innerHTML = sessionStorage.???;
+		document.getElementById('time_one').innerHTML = sessionStorage.???;
+		
+		
+	//Get Second ranking information from algorithm
+		document.getElementById('travel_two').innerHTML = sessionStorage.???;
+		document.getElementById('money_two').innerHTML = sessionStorage.???;
+		document.getElementById('sust_two').innerHTML = sessionStorage.???;
+		document.getElementById('time_two').innerHTML = sessionStorage.???;
+		
+	//Get Third ranking information from algorithm
+		document.getElementById('travel_three').innerHTML = sessionStorage.???;
+		document.getElementById('money_three').innerHTML = sessionStorage.???;
+		document.getElementById('sust_three').innerHTML = sessionStorage.???;
+		document.getElementById('time_three').innerHTML = sessionStorage.???;
+	*/
+	
+	
+	document.getElementById('travel_one').innerHTML = "Lightrail";
+		document.getElementById('money_one').innerHTML = "15";
+		document.getElementById('sust_one').innerHTML = "100";
+		document.getElementById('time_one').innerHTML = "45";
+		
+		
+	//Get Second ranking information from algorithm
+		document.getElementById('travel_two').innerHTML = "Uber";
+		document.getElementById('money_two').innerHTML = "30";
+		document.getElementById('sust_two').innerHTML = "200";
+		document.getElementById('time_two').innerHTML = "20";
+		
+	//Get Third ranking information from algorithm
+		document.getElementById('travel_three').innerHTML = "Carpool";
+		document.getElementById('money_three').innerHTML = "10";
+		document.getElementById('sust_three').innerHTML = "150";
+		document.getElementById('time_three').innerHTML = "20";
+}	
